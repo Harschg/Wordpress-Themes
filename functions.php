3a123dbad3c0d110ff07c693e2c56f096aaa4e42
Grant Harsch <grant.harsch@anchordistributors.com>
2026-09-24 12:20:51 -0400

    fixed some graphical bugs

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'STILLFRAME_VERSION', '1.4.2' );
+define( 'STILLFRAME_VERSION', '1.4.3' );
 
 require get_template_directory() . '/inc/setup.php';
 require get_template_directory() . '/inc/post-types.php';
